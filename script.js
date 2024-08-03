f4921e159b1378b2c812586df78cbf747ccdce54
garv2401 <118100178+garv2401@users.noreply.github.com>
2024-08-03 12:12:17 +0530

    Update script.js
    ..

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ async function displayAlbums(){
             let folder=e.href.split("/").slice(-1)[0];
             if(folder!="songs"){
 
-            let a=await fetch(`http://127.0.0.1:5500/Spotify/songs/${folder}/info.json`);
+            let a=await fetch(`https://127.0.0.1:5500/Spotify/songs/${folder}/info.json`);
             let response=await a.json();
             
 
@@ -160,7 +160,7 @@ async function displayAlbums(){
 
                         <div class="play">
                             <div class="svg-container">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                                     <circle cx="12" cy="12" r="12" class="circle-background"/>
                                     <svg x="2" y="2" width="26" height="26" viewBox="0 0 30 30">
                                         <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" class="play-button"/>
@@ -169,7 +169,7 @@ async function displayAlbums(){
                             </div>           
                             
                         </div>
-                        <img src="http://127.0.0.1:5500/Spotify/songs/${folder}/cover.jpg" alt="">
+                        <img src="https://127.0.0.1:5500/Spotify/songs/${folder}/cover.jpg" alt="">
                         <h2>${response.title}</h2>
                         <p>${response.description}</p>
                     </div>`
